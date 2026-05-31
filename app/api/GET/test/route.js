87fc910558356b54db249052d3dae18f888c18d9
veerapat <veeraput.kb09834@gmail.com>
2026-05-31 21:54:38 +0700

    fix_api_request

--- app/api/GET/test/route.js
+++ app/api/GET/test/route.js
@@ -4,6 +4,6 @@ export async function GET() {
     const [rows] = await pool.query("SELECT * FROM users");
     return Response.json(rows);
   } catch (error) {
-    console.log(error);
+    return Response.json({ error: error.message }, { status: 500 });
   }
 }
--- app/api/POST/getlogin/route.js
+++ app/api/POST/getlogin/route.js
@@ -1,13 +1,18 @@
 import { pool } from "@/db";
+
 export async function POST(request) {
-  const { email } = await request.json();
   try {
+    const { email } = await request.json();
+
     const [rows] = await pool.query(
       "SELECT * FROM users WHERE staff_email = ?",
       [email],
     );
+
     return Response.json(rows);
   } catch (error) {
     console.error("Database query error:", error);
+
+    return Response.json({ error: error.message }, { status: 500 });
   }
 }
